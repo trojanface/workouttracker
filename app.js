83c3d4666a9b4171a38c78bc3d586526917f3d3a
MattT <trojanface@hotmail.com>
2020-06-18 18:42:43 +0930

    Basic express server

---
+++ app.js
@@ -0,0 +1,15 @@
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+let db = require("./models");
+
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+require("./controllers/workout-controller.js")(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+})
